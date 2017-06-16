15c15824f9a058cba11179f3fd2b6994ddc8b066
A <usernam@users.noreply.github.com>
2017-06-15 21:08:28 -0400

    models done

--- common/net/triplegerms/unitygear/item/ArmorBase.java
+++ common/net/triplegerms/unitygear/item/ArmorBase.java
@@ -1,11 +1,12 @@
 package net.triplegerms.unitygear.item;
 
-import net.minecraft.init.SoundEvents;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.common.util.EnumHelper;
 import net.triplegerms.unitygear.UnityGear;
 import net.triplegerms.unitygear.lib.Names;
 
@@ -23,6 +24,11 @@ public class ArmorBase extends ItemArmor {
 	@Override
 	public String getUnlocalizedName(ItemStack stack) {
 		return "item." + UnityGear.RESOURCE_PREFIX + Names.UNITY_DIAMOND_ITEM;
+	}
+	
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack){
+		
 	}
 
 }
